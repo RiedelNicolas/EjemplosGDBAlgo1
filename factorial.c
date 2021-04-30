7122787fb2fbd37128b3a547433b611cd6d80239
RiedelNicolas <nariedel99@gmail.com>
2021-04-30 19:44:37 -0300

    ponemos mas lindo el primer ej con steph

--- factorial.c
+++ factorial.c
@@ -1,30 +1,43 @@
 #include <stdio.h>
 
+// Declaracion de firmas.
 
+/*
+*  Recibe un entero positivo, y devuelve su factorial.
+*/
+unsigned int calcular_factorial(unsigned int numero_ingresado);
+/*
+* Solicita al usuario el ingreso de un numero por pantalla.
+*/
+void solicitar_datos();
+/*
+* Muestra el entero recibido por pantalla.
+*/
+void mostrar_resultado(unsigned int resultado);
 
-unsigned int factorial(int n);
-
-
-int main(){
-	int n = 0;
+int main() {
+	unsigned int numero_ingresado = 0;
 	unsigned int resultado = 0;
-	printf("\nIngrese el numero del que desea calcular el factorial\n");
-	scanf("%i",&n);
-
-	resultado = factorial(n);
-
-	printf("El resultado es %u \n",resultado);
-
+	scanf("%u", &numero_ingresado);
+	resultado = calcular_factorial(numero_ingresado);
 	return 0;
 }
 
+void solicitar_datos () {
+	printf("\nIngrese el numero del que desea calcular el factorial\n");
+}
 
-unsigned int factorial(int n){
+void mostrar_resultado(unsigned int resultado) {
+	printf("El resultado es %u \n",resultado);	
+}
+
+unsigned int calcular_factorial(unsigned int numero_ingresado){
 	unsigned int resultado = 1;
 
-    while(n!=0){
-    	n--;
-        resultado*=n;
+    while(numero_ingresado != 0){
+    	numero_ingresado--;
+        resultado*=numero_ingresado;
     }
+
     return resultado;
 }
